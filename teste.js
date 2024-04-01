871ca10c49707dd613c8c0ed859683790492b1c1
Patrinysilva <patrinysilva17@gmail.com>
2024-04-01 07:22:36 -0300

    teste

---
+++ teste.js
@@ -0,0 +1,15 @@
+let url = 'http://localhost:5000/agendamento/teste'
+fetch(url)
+    .then
+
+
+function getInputValue(){
+    var Nome1 = documentgetElementById("nome").value;
+    console.log("Seu nome: "+Nome1)
+}
+
+
+
+
+
+
